87a0df259b1fbb1d6fa6dcd932fb87998bd22114
Your Name <you@example.com>
2022-11-13 13:46:57 +0100

    second

--- variables.tf
+++ variables.tf
@@ -1,11 +1,20 @@
 variable "persons" {
   description = "Please enter the Map list of the persons "
-  type        = map(any)
+  type        = map(list(string))
 
 
   validation {
     condition = alltrue([
     for person_list in var.persons : length(person_list) == 3])
-    error_message = "The lists of the MAP must contains 3 elements (Prenom , Non , Num)"
+    error_message = "The lists of the MAP must contains 3 elements (Prenom , Nom , Phone number)"
   }
+
+
+  validation {
+    condition = alltrue([
+    for person_list in var.persons : can(regex("^[0-9]+$", element(person_list, 2)))])
+    error_message = "The 3 element of every map list Must be a phone number (Ex:0656987..) !"
+  }
+
+
 }
